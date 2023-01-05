fb34e2290451f5cdaa66cce25bbcc6dbd9e722bc
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-05 23:30:57 +0530

    Add files via upload

---
+++ ReadData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace InventoryListManangement
+{
+    public class ReadData
+    {
+
+        public InventoryModel Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoryModel>(json);
+
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+
+
+    }
+}
